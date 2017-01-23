2db4e21d6864a1d4c21f291f40581ecdb0fa2285
joshuakcockrell <joshuakcockrell@gmail.com>
2017-01-23 09:43:06 -0700

    good

--- commit.py
+++ commit.py
@@ -3,8 +3,8 @@ import urllib2
 import random
 import time
 
-apiUrl = "https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=0&format=text"
-apiUrl = "http://www.setgetgo.com/randomword/get.php"
+apiUrl = 'https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=0&format=text'
+apiUrl = 'http://www.setgetgo.com/randomword/get.php'
 
 words = ['uhh', 'what the..', 'okay', 
 		 'help', 'not sure', 'yep', 
@@ -12,7 +12,10 @@ words = ['uhh', 'what the..', 'okay',
 		 'well then', 'nope', 'dont ask',
 		 'changed somethingggg', 'yeah',
 		 'my site is lit', 'too lit to quit',
-		 'all I do is bleh', 'dont look at me']
+		 'all I do is bleh', 'dont look at me',
+		 'not sure what this is', 'uhhh', 'uhhhh',
+		 'my site is legit', 'good', 'ohh', 'hi',
+		 'k', 'stuff']
 
 
 def run():
@@ -27,17 +30,24 @@ def run():
 	open('test.txt', 'w').close()
 
 	# Write to file
-	with open("test.txt", "a") as myfile:
+	with open('test.txt', 'a') as myfile:
 	    myfile.write(randomWord)
 
 	# Commit
-	# os.system("git status")
-	os.system("git add -A")
-	os.system("git commit -m " + commitWord)
-	os.system("git push")
+	# os.system('git status')
+	os.system('git add -A')
+	time.sleep(10)
+	os.system('git commit -m ' + commitWord)
+	time.sleep(10)
+	os.system('git push')
 
-
-
+print 'Running scheduler..'
 while True:
+	print 'Ran at: ' + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 	run()
-	time.sleep(60)
+	wait = 60 * 60 * 24 * random.uniform(0.5, 2)
+	print 'Seconds till next run: ' + str(wait)
+	time.sleep(wait)
+
+
+
