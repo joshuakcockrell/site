b77b7d3ced278f58957c0429479a102be91d2530
joshuakcockrell <joshuakcockrell@gmail.com>
2017-01-23 09:24:34 -0700

    okay

--- commit.py
+++ commit.py
@@ -1,13 +1,19 @@
 import os
 import urllib2
+import random
 
 os.system("git status")
 
 apiUrl = "https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=0&format=text"
 apiUrl = "http://www.setgetgo.com/randomword/get.php"
 
+words = ['uhh', 'what the..', 'okay', 
+		 'help', 'not sure', 'yep', 
+		 'k thanks', 'made sense to me', 
+		 'well then', 'nope', 'dont ask']
+
 randomWord = urllib2.urlopen(apiUrl).read()
-commitWord = urllib2.urlopen(apiUrl).read()
+commitWord = random.choice(words)
 
 # Wipe contents
 open('test.txt', 'w').close()
